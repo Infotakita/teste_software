46d009045d487b97fa4febc0bbb1afb85a9c0762
Infotakita <infotakita@gmail.com>
2024-04-12 11:17:44 -0300

    Projeto ADS5B

---
+++ recebeDados.test.js
@@ -0,0 +1,12 @@
+
+const dadosEstudante = require('./dadosEstudante');
+
+describe('Teste da função recebeDados', () => {
+  test('Verifica se a função processa corretamente o nome "Paulo Takita" e o RA "184189"', () => {
+    const nome = "Paulo Takita";
+    const RA = "184189";
+    const esperado = `Dados recebidos - Nome: ${nome}, RA: ${RA}`;
+
+    expect(dadosEstudante(nome, RA)).toBe(esperado);
+  });
+});
